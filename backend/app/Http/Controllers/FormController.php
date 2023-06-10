604257dc775f0041af9d62028e88693d926c2ecd
ahmadfirdaus22 <ahmadfirdaus22>
2023-06-10 15:03:48 +0700

    Make all Route and also make controller for all the step

--- backend/app/Http/Controllers/FormController.php
+++ backend/app/Http/Controllers/FormController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Form;
 use App\Models\FormProgram;
+use App\Models\FormSubject;
 use App\Models\ProgramSubject;
 use App\Models\Subject;
+use App\Models\SubjectCause;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -66,7 +68,23 @@ class FormController extends Controller
      */
     public function steptwo(Request $request)
     {
-
+        $validated = $request->validate([
+            'subjects' => 'array|min:6|required',
+            'form_id' => 'required'
+        ]);
+        $subjects  = $validated['subjects'];
+        foreach($subjects as $sub){
+            $data = [
+                'subject_id' => $sub,
+                'form_id' => $validated['form_id']
+            ];
+            FormSubject::create($data);
+        }
+        $form = Form::where('id', $validated['form_id'])->get();
+        return response()->json([
+            'status' => 'OK',
+            'data' => $form
+        ]);
     }
     /**
      * Display the Recommended and Required
@@ -77,16 +95,16 @@ class FormController extends Controller
         $alldata = [];
         foreach($form as $fo){
             $data = DB::table('program_subject')
-                        ->select('subject_name')
+                        ->select('name')
                         ->where('program_id', $fo['program_id'])
                         ->join('subjects', 'program_subject.subject_id', '=', 'subjects.id')
                         ->get();
             foreach($data as $da){
-                
-                $alldata +=$data->subject_name;
+                array_push($alldata,$da->name);
             }
         }
-        dd($alldata);
+        $alldata = array_unique($alldata);
+        $alldata = array_values($alldata);
 
         return response()->json([
             'status' => 'OK',
@@ -97,24 +115,105 @@ class FormController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Form $form)
+    public function show_fs(Form $id)
     {
-        //
+        $data = DB::table('form_subject')
+                    ->select('subject_id','name')
+                    ->where('form_id', $id['id'])
+                    ->join('subjects', 'form_subject.subject_id', '=', 'subjects.id')
+                    ->get();
+
+        return response()->json([
+            'status' => 'OK',
+            'data' => $data
+        ]);
     }
 
     /**
-     * Update the specified resource in storage.
+     * Create the subject Cause 
      */
-    public function update(Request $request, Form $form)
+    public function stepthree(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'causes' => 'array|required',
+            'form_id' => 'required'
+        ]);
+        $causes = $validated['causes'];
+        foreach($causes as $cas){
+            $data = [
+                'form_id' => $validated['form_id'],
+                'subject_id' => $cas['subject_id'],
+                'isGood' => $cas['isGood'],
+                'isInterested' => $cas['isInterested'],
+                'isRequired' => $cas['isRequired']
+            ];       
+            SubjectCause::create($data);
+        }
+
+        $form = Form::where('id', $validated['form_id'])->get();
+        
+        return response()->json([
+            'status' => 'OK',
+            'data' => $form
+        ]);
+
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Save the meeting person and time
      */
-    public function destroy(Form $form)
+    public function stepfour(Request $request)
+    {   
+        $validated = $request->validate([
+            'form_id' =>  'required',
+            'meeting_time' => 'required',
+            'meeting_person' => 'required'
+        ]);
+        $id = $validated['form_id'];
+        unset($validated['form_id']);
+        Form::where('id', $id)->update($validated);
+        $form =   Form::where('id', $id)->get();
+        return response()->json([
+            'status' => 'OK',
+            'data' => $form
+        ]);
+    }
+
+    /**
+     * Get All data for the final
+     */
+
+    public function finalstep(Form $id)
     {
-        //
+        $program = DB::table('form_program')
+                    ->select('name')
+                    ->where('form_id',$id['id'])
+                    ->join('programs', 'form_program.program_id', '=', 'programs.id')
+                    ->get();
+        $data1= [];
+        foreach($program as $pro){
+           array_push($data1,$pro->name);
+        }
+
+        $subject = DB::table('form_subject')
+                    ->select('name')
+                    ->where('form_id',$id['id'])
+                    ->join('subjects', 'form_subject.subject_id', '=', 'subjects.id')
+                    ->get();
+        $data2 = [];
+        foreach($subject as $sub){
+           array_push($data2,$sub->name );
+        }
+        
+
+        $alldata=[
+            'programs' => $data1,
+            'subjects'=> $data2,
+            'form' => $id
+        ]; 
+        return response()->json([
+            'status' => 'OK',
+            'data' =>  $alldata
+        ]);
     }
 }
